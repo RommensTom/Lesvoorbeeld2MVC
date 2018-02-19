af4a84238ecf92fa4dec63d0099c0b024306e230
Tomtom8434 <tomromm@hotmail.com>
2018-02-19 11:31:53 +0100

    Updatje simpel naampje

---
+++ Lesvoorbeeld2MVC/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesvoorbeeld2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string name)
+        {
+            return $"Hallo {name}";
+        }
+    }
+}
